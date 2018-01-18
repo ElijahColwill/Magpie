7728cb085c8dd77f9a499b397daa5ffa0d4ea948
EljiahColwill <elijah.c035@gmail.com>
2018-01-18 12:14:47 -0700

    Activity 2

--- src/Magpie2.java
+++ src/Magpie2.java
@@ -31,13 +31,27 @@ public class Magpie2 {
 	 */
 	public String getResponse(String statement) {
 		String response = "";
-		if (statement.indexOf("no") >= 0) {
+		String statementTrim = statement.trim();
+		if (statementTrim.length() < 1) {
+			response = "Sorry, say that again?";
+		} else if (statement.indexOf("no") >= 0) {
 			response = "Why so negative?";
 		} else if (statement.indexOf("mother") >= 0
 				|| statement.indexOf("father") >= 0
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0) {
 			response = "Tell me more about your family.";
+		} else if (statement.indexOf("cat") >= 0
+				|| statement.indexOf("dog") >= 0) {
+			response = "Tell me more about your pets.";
+		} else if (statement.indexOf("Padjen") >= 0) {
+			response = getBadResponse();
+		} else if (statement.indexOf("weather") >= 0) { 
+			response = "I hope we get snow. Winter is my favorite season.";
+		} else if (statement.indexOf("Rockies") >= 0) {
+			response = "You have poor taste in baseball teams.";
+		} else if (statement.indexOf("kms") >= 0) {
+			response = "You should see a therapy dog.";
 		} else {
 			response = getRandomResponse();
 		}
@@ -50,7 +64,7 @@ public class Magpie2 {
 	 * @return a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -63,8 +77,32 @@ public class Magpie2 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (whichResponse == 4) {
+			response = "Relly? I didn't know that.";
+		} else if (whichResponse == 5) {
+			response = "Cool.";
 		}
 
 		return response;
 	}
+
+	private String getBadResponse() {
+		final int NUMBER_OF_RESPONSES = 4;
+		double r = Math.random();
+		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
+		String response = "";
+
+		if (whichResponse == 0) {
+			response = "Mr. Padjen is good.";
+		} else if (whichResponse == 1) {
+			response = "Mr. Padjen seems like a cool guy";
+		} else if (whichResponse == 2) {
+			response = "Padjen's okay, I guess.";
+		} else if (whichResponse == 3) {
+			response = "That's the computer science teacher, right";
+		}
+
+		return response;
+	}
+
 }
